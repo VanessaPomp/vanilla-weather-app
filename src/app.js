9265293db28795105cbe0c033057d3801ca0f099
VanessaPomp <vanessa.pomp@yahoo.de>
2021-09-25 22:48:03 +0200

    Showing the currrent temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
-alert("Hello World")
+function displayTemperature(response) {
+    console.log(response.data);
+    let temperatureElement = document.querySelector("#temperature");
+    let cityElement = document.querySelector("#city")
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    cityElement.innerHTML = response.data.name;
+    descriptionElement.innerHTML = response.data.weather[0].description;
+    humidityElement.innerHTML = response.data.main.humidity;
+    windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+
+let apiKey = "45a134233c7cdfc7dd5c67fe467c947d";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Chicago&appid=${apiKey}&units=metric`
+
+axios.get(apiUrl).then(displayTemperature);
